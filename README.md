ba0afb5139505ad499572f8a420c71f84c35b043
pfts <tertunight@yahoo.com.br>
2022-06-07 16:32:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-jnu8ab
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-jnu8ab)
---
+++ pages/extra/extra.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-extra',
+  templateUrl: 'extra.html'
+})
+export class ExtraPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
